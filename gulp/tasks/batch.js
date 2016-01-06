3598c6e00511540637b2dd1da958a9f2fa0770f3
Tarah Soljan <tarah@withcollective.com>
2016-01-06 16:02:11 +1100

    favicons batch config

--- gulp/tasks/batch.js
+++ gulp/tasks/batch.js
@@ -16,20 +16,7 @@ var rename          = require('gulp-rename');
 var tinypng         = require('gulp-tinypng-compress');
 
 
-var images = [
-    { rename: 'mstile-558x558', width: 558, crop: false },
-    { rename: 'mstile-558x270', width: 558, height: 270, crop: true },
-    { rename: 'mstile-270x270', width: 270, height: 270, crop: true },
-    { rename: 'mstile-128x128', width: 128, height: 128, crop: true },
-    { rename: 'icon-192x192', width: 192, height: 192, crop: true },
-    { rename: 'icon-144x144', width: 144, height: 144, crop: true },
-    { rename: 'icon-96x96', width: 96, height: 96, crop: true },
-    { rename: 'icon-72x72', width: 72, height: 72, crop: true },
-    { rename: 'icon-48x48', width: 48, height: 48, crop: true },
-    { rename: 'icon-36x36', width: 36, height: 36, crop: true },
-    { rename: 'favicon-192x192', width: 192, height: 192, crop: true },
-    { rename: 'apple-touch-icon-192x192', width: 192, height: 192, crop: true },
-];
+
 
 /**
  * @task batch
@@ -38,7 +25,7 @@ var images = [
  */
 gulp.task('batch', function () {
 
-  images.forEach(function(type){
+  favicons.forEach(function(type){
 
       var resize_settings = {
           width: type.width,
