eabbf07edde9c49c5ec87d76a5e5c0e8463129fb
Tarah Soljan <tarah@withcollective.com>
2016-01-08 16:53:14 +1100

    kit notify messages

--- gulp/tasks/kit.js
+++ gulp/tasks/kit.js
@@ -6,10 +6,22 @@
 var gulp            = require('gulp');
 var config          = require('../config');
 var kit             = require('gulp-kit');
+var plumber         = require('gulp-plumber');
 var notify          = require('gulp-notify');
 var plumber         = require('gulp-plumber');
 var browserSync     = require('browser-sync');
 var reload          = browserSync.reload;
+
+
+kitError = function (error) {
+  notify({
+     title: 'Error: [' + error.plugin + ']',
+     message: error.message + 'File: ' + error.Line,
+     sound: 'Funk',
+   }).write(error);
+   console.log(config.chalkStyles.eTitle.white.bgRed('REPORT : Error in ' + error.plugin ) + '\n' + config.chalkStyles.eTitle('MSG: ') + config.chalkStyles.eMsg(error.message) + '\n' );
+  this.emit('end');
+};
 
 
 gulp.task('kit', function () {
