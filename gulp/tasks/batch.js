27d0c2e988e24f2c6c06bace7d08a8db2e85ec51
tarah soljan <Tcharmian@gmail.com>
2016-01-09 10:09:34 +1100

    gulp batch comments update

--- gulp/tasks/batch.js
+++ gulp/tasks/batch.js
@@ -6,14 +6,14 @@
 * Batch image crop and compress with Tinypng, Image Resize and Rename
 * gulp-tinypng-compress: https://www.npmjs.com/package/gulp-tinypng
 * gulp-image-resize: https://www.npmjs.com/package/gulp-image-resize
-* gulp-rename: https://www.npmjs.com/package/gulp-rename
-
+* gulp-rename: https://www.npmjs.com/package/gulp-rename 
+* Based on ryantbrown: https://gist.github.com/ryantbrown/239dfdad465ce4932c81
 * Requirements
 * Tinypng API Key: https://tinypng.com/developers/
 **/
 
-var gulp            = require('gulp');
 var config          = require('../config');
+var gulp            = require('gulp');
 var imageresize     = require('gulp-image-resize');
 var rename          = require('gulp-rename');
 var tinypng         = require('gulp-tinypng-compress');
